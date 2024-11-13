8be20833862a13c248ad4cc3fd4537f3932ce5cd
Navid M <navidmomtahen@gmail.com>
2024-11-13 00:33:40 +0000

    comments

--- src/sequence.nim
+++ src/sequence.nim
@@ -6,19 +6,15 @@ import
     tables
   ]
 
-type SequenceItem* = ref object of RootRef
+type
+  SequenceItem* = ref object of RootRef
 
-type DelayBlock* = ref object of SequenceItem
-  beats*: float32
+  DelayBlock* = ref object of SequenceItem
+    beats*: float32
 
-type NoteBlock* = ref object of SequenceItem
-  note*: Note
-  octave*: int
-
-proc addAll*(items: seq[SequenceItem], toAdd: varargs[SequenceItem]): seq[SequenceItem] =
-  result = items
-  for addable in toAdd:
-    result.add(addable)
+  NoteBlock* = ref object of SequenceItem
+    note*: Note
+    octave*: int
 
 let stringsToNotes: Table[string, Note] = {
   "A": Note.A,
@@ -27,13 +23,25 @@ let stringsToNotes: Table[string, Note] = {
   "D": Note.D,
   "E": Note.E,
   "F": Note.F,
-  "G": Note.G
+  "G": Note.G,
+  "G#": Note.Gs,
+  "F#": Note.Fs,
+  "A#": Note.As,
 }.toTable
 
+proc addAll*(items: seq[SequenceItem], toAdd: varargs[SequenceItem]): seq[SequenceItem] =
+  ## Add all of the given sequence items to the sequence
+  result = items
+  for addable in toAdd:
+    result.add(addable)
+
 proc single*(noteName: string): NoteBlock =
+  ## Get back some note item
   let
     noteToUse = noteName[0]
     octaveToUse = parseInt($noteName[1])
   return NoteBlock(note: stringsToNotes[$noteToUse], octave: octaveToUse)
 
-proc wait*(beats: float32): DelayBlock = DelayBlock(beats: beats)
+proc wait*(beats: float32): DelayBlock =
+  ## Get back some delay item
+  DelayBlock(beats: beats)
