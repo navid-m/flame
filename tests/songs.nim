ba31633e494a846c2e1005d3d487b0d4e231741d
Navid M <navidmomtahen@gmail.com>
2024-11-14 19:53:51 +0000

    smb theme

--- tests/songs.nim
+++ tests/songs.nim
@@ -3,10 +3,37 @@ import flame
 var song: seq[SequenceItem]
 
 song = song.addAll(
-  chord("C4", "D5"),
-  wait(1),
-  chord("C#4", "D#5"),
-  wait(1),
+  single("E5"), wait(0.5),
+  single("E5"), wait(0.5),
+  single("E5"), wait(0.5),
+  single("C5"), wait(0.5),
+  single("E5"), wait(0.5),
+  single("G5"), wait(1.0),
+
+  wait(0.5),
+
+  single("G4"), wait(1.0),
+  single("C5"), wait(0.5),
+  single("G4"), wait(0.5),
+  single("E4"), wait(0.5),
+  single("A4"), wait(0.5),
+  single("B4"), wait(0.5),
+  single("A#4"), wait(0.5),
+  single("A4"), wait(0.5),
+
+  single("G4"), wait(0.5),
+  single("E5"), wait(0.5),
+  single("G5"), wait(0.5),
+  single("A5"), wait(1.0),
+  single("F5"), wait(0.5),
+  single("G5"), wait(0.5),
+
+  wait(0.5),
+
+  single("E5"), wait(0.5),
+  single("C5"), wait(0.5),
+  single("D5"), wait(0.5),
+  single("B4"), wait(1.0)
 )
 
 play(song)
